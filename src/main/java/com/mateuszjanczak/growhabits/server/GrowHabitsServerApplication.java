af8380a7dafc07c8510df02a987e9072e6394b9a
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2020-08-22 02:11:15 +0200

    spring initial commit

---
+++ src/main/java/com/mateuszjanczak/growhabits/server/GrowHabitsServerApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.growhabits.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowHabitsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowHabitsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuszjanczak/growhabits/server/GrowHabitsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.growhabits.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowHabitsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
